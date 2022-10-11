1ff2fd2c8ddcc63c6e1ce0680d235b5fba54620d
https://github.com/bhtanvin0 <bhtanvin1836@gmail.com>
2022-10-11 23:08:23 +0600

    showingDataOnHomePage

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
     const quizsData = loaderData.data
     
     return (
-        <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
+        <div className=' px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <div className='grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2'>
         {quizsData.map(quiz => (
-          <Quiz key={quiz.isbn13} quiz={quiz}></Quiz>
+          <Quiz key={quiz.id} quiz={quiz}></Quiz>
         ))}
       </div>
     </div>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const Quiz = () => {
+const Quiz = ({ quiz }) => {
+    const { id, logo, name, total, } = quiz
     return (
-        <div>
-            This is quiz
+      <a href={`../quiz/${id}`} aria-label='View Item'>
+        <div className='relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl'>
+          <img
+            className=' bg-slate-400 w-full h-56 md:h-64 xl:h-80'
+            src={logo}
+            alt=''
+          />
+          <div className=''>
+            <p className='mb-4 text-lg font-bold text-black-100'>{name}</p>
+            <br />
+            <p className='text-sm tracking-wide text-black-300'>Price: {total}</p>
+          </div>
+          <button className='m-4 bg-yellow-200 p-4'>Start Practice</button>
         </div>
-    );
-};
+      </a>
+    )
+  }
 
 export default Quiz;
