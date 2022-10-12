cfdca10c38057e77869102162fa010d81cad476f
https://github.com/bhtanvin0 <bhtanvin1836@gmail.com>
2022-10-12 12:30:01 +0600

    add a hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,34 @@ const Home = () => {
     const quizsData = loaderData.data
     
     return (
-        <div className=' px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
+        <div>
+            
+<div class="bg-white dark:bg-gray-800 overflow-hidden relative m-20">
+    <div class="text-start w-1/2 py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+        <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+            <span class="block">
+            Ensure that you are aware
+            </span>
+            <span class="block text-indigo-500">
+                It&#x27;s today or never.
+            </span>
+        </h2>
+        
+    </div>
+    <img src="https://i.natgeofe.com/k/f5b0d18e-78f4-4697-bd3f-509aaa3bf606/1900x1068_herolead_quiz_square.jpg" class="absolute h-full w-1/4 hidden lg:block right-0 top-0"/>
+</div>
+
+
+
+            <div className=' px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <div className='grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2'>
         {quizsData.map(quiz => (
           <Quiz key={quiz.id} quiz={quiz}></Quiz>
         ))}
       </div>
     </div>
+        </div>
+      
     );
 };
 
--- src/components/QuizAns.js
+++ src/components/QuizAns.js
@@ -3,6 +3,9 @@ import React from 'react';
 
 const QuizAns = ({question}) => {
     const {question:qz, options, correctAnswer, id} = question;
+
+
+
     
     return (
         <div className='m-20 border-4 bg-blue-500 p-3 my-3 '>
@@ -10,7 +13,7 @@ const QuizAns = ({question}) => {
             <div >
                 {
                     options.map(option =>
-                    <li option= {option} className='border-3 border-blue-300 p-3 hover:bg-slate-400 text-center text-white'> {option}</li>
+                <li option= {option} className='border-3 border-blue-300 p-3 hover:bg-slate-400 text-center text-white'> {option}</li>
                     )
                 }
             </div>
