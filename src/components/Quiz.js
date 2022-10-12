e964eeedc2edb7d9be033bbc7b767cb82c9b9258
https://github.com/bhtanvin0 <bhtanvin1836@gmail.com>
2022-10-12 11:13:15 +0600

    few things design

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,19 +5,19 @@ const Quiz = ({ quiz }) => {
     const { id, logo, name, total, } = quiz
     return (
        
-        <div className='h-[450px] p-2 relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl'>
+        <div className='h-[480px] p-2 relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl'>
           <img
             className=' bg-slate-400 w-full h-56 md:h-64 xl:h-80'
             src={logo}
             alt=''
           />
-         <div>
+         <div className=' items-center'>
          <div className=''>
-            <p className='mb-8 text-lg font-bold text-center text-black-100'>{name} | total{total} </p>
+            <p className='mb-8 text-lg font-bold text-left text-black-100 mt-5'>{name} | total{total} </p>
         
           </div>
 
-          <Link to= {`/quiz/${id}`} className=' bg-yellow-200 rounded-lg p-4 m-12 shadow-lg  hover:shadow-2xl'>Start Practice</Link>
+          <Link to= {`/quiz/${id}`} className='inline-flex items-center justify-center w-full h-12 px-6 mb-20 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-blue-400 hover:bg-blue-700 focus:shadow-outline focus:outline-none'>Start Practice</Link>
          </div>
         </div>
 
--- src/components/QuizAns.js
+++ src/components/QuizAns.js
@@ -1,17 +1,16 @@
-import { list } from 'postcss';
+
 import React from 'react';
 
 const QuizAns = ({question}) => {
     const {question:qz, options, correctAnswer, id} = question;
-    console.log(correctAnswer)
     
     return (
-        <div className='border-4 border-red-400 p-3 my-3'>
-            <h1>Quiz: {qz}</h1>
-            <div>
+        <div className='m-20 border-4 bg-blue-500 p-3 my-3 '>
+            <h1 className='mb-4 text-xl font-medium text-center text-white'>Quiz: {qz}</h1>
+            <div >
                 {
                     options.map(option =>
-                    <li option= {option} className='border-3 border-blue-300 p-3 hover:bg-orange-800 text-center'> {option}</li>
+                    <li option= {option} className='border-3 border-blue-300 p-3 hover:bg-slate-400 text-center text-white'> {option}</li>
                     )
                 }
             </div>
